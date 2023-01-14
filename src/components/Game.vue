2a5720213dcb11cfaac30fe073fa2aaf9fd8a3e3
kevinmorris1 <kdm0030@mix.wvu.edu>
2023-01-14 10:18:59 -0500

    Basic Framework

---
+++ src/components/Game.vue
@@ -0,0 +1,34 @@
+<script>
+import Video from "./Video.vue";
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  components: { Video },
+};
+</script>
+
+<template>
+  <div class="card">
+    <button @click="count++">You clicked me {{ count }} times.</button>
+    <Video />
+  </div>
+</template>
+
+<style scoped>
+.card {
+  width: 60vw;
+  height: 75vh;
+  padding: 15px;
+  box-sizing: border-box;
+  background-color: var(--COOL_LIGHT);
+  border-radius: 10px;
+}
+
+button {
+  width: 100%;
+  
+}
+</style>
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="page">
+    <Game />
+  </div>
+</template>
+<script>
+import Game from '../components/Game.vue';
+export default {
+  name: "LandingPage",
+  components: { Game},
+};
+</script>
+<style>
+.page {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--LIGHT);
+}
+</style>

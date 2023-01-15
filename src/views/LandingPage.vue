969e9765f697341d79e0080656db18d110b3e4ae
kevinmorris1 <kdm0030@mix.wvu.edu>
2023-01-15 09:13:31 -0500

    Update

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,22 +1,10 @@
 <template>
-  <div class="page">
-    <Game />
-  </div>
+  <div class="page">Hello World</div>
 </template>
 <script>
-import Game from '../components/Game.vue';
 export default {
   name: "LandingPage",
-  components: { Game},
+  components: {},
 };
 </script>
-<style>
-.page {
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: var(--LIGHT);
-}
-</style>
+<style></style>
